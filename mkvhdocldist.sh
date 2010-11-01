5fd739304e400649670ce2a33db2df8f09df0b71
Volker Schatz <vs@127.0.0.1>
2010-11-01 18:25:55 +0100

    Distribution tarball generation script now deletes tmp files of pod2html.

--- mkvhdocldist.sh
+++ mkvhdocldist.sh
@@ -4,6 +4,8 @@ ver=`grep '^my $version' vhdocl | sed 's/.*"\(.*\)".*/\1/'`
 
 cp vhdocl vhdocl-dist/
 
+rm vhdocl-dist/pod2htm?.tmp
+
 mv vhdocl-dist vhdocl-"$ver"
 
 tar czf vhdocl-"$ver".tgz vhdocl-"$ver"
