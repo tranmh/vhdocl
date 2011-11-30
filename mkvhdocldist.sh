2f45fcfdb0d2c7ccb7ff46d476a68e7a24502f68
Volker Schatz <vs@127.0.0.1>
2011-11-30 20:29:13 +0100

    Changed packaging script to use git-describe instead of parsing git-log output.

--- mkvhdocldist.sh
+++ mkvhdocldist.sh
@@ -4,15 +4,8 @@ if [ -n "$1" ]; then
     commit=$1
     echo "Packaging current version as $commit"
 else
-    commit=`git log --decorate=short | head -n 1`
-    if echo $commit | grep -q '\<tag: ' ; then
-        commit=`echo $commit | sed -e 's/^.*\<tag: \([^,]\+\).*$/\1/'`
-        echo "Packaging version $commit"
-    else
-        commit="${commit##commit }"
-        commit=${commit:0:8}
-        echo "Packaging commit $commit"
-    fi
+    commit=`git describe --tags`
+    echo "Packaging version $commit"
 fi
 
 sed -e 's/^my \$version\>.*$/my $version= "'"$commit"'";/' vhdocl > vhdocl-dist/vhdocl
