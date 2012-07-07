501017c12f3b3e9ae322eac5df83cca3bdd59640
Volker Schatz <vs@127.0.0.1>
2012-07-07 16:43:40 +0200

    New install script for installing from git repository.
    This does the necessary steps of mkvhdocldist.sh (copying the .pod and
    substituting the current version in vhdocl) and exec's the installer in the
    distribution directory.

---
+++ install-from-git.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl -w
+
+use strict;
+use File::Copy;
+
+my $usage= <<EOF;
+usage: install-from-git.pl [ -n ] [ DATADIR=<directory> ] [ <installation dir> ]
+-n      Update package directory, but only pretend to install
+DATADIR Target directory for style files
+The installation directory is the base directory for the executable and most
+other files.  Use -n to view the individual install paths.
+EOF
+
+if( grep /^(?:-h|--?help)$/i, @ARGV ) {
+    print $usage;
+    exit;
+}
+
+print "Copying current version to package directory...\n";
+
+copy("./pod/vhdocl.pod", "./vhdocl-dist") or die
+   "Could not copy .pod documentation - are you in the repository directory?";
+
+my $commit= `git describe --tags`;
+if( $? ) {
+    print STDERR "Warning: Could not determine git revision\n";
+    $commit= undef;
+}
+else {
+    chomp $commit;
+}
+
+my $script;
+{
+    local $/;
+    open SCR, "<vhdocl" or die "Could not read current vhdocl version";
+    $script= <SCR>;
+    close SCR;
+}
+
+$script =~ s/^my \$version\b.*$/my \$version= "$commit";/m if $commit;
+
+open SCR, ">./vhdocl-dist/vhdocl" or die "Could not write vhdocl to package directory";
+print SCR $script;
+close SCR;
+
+chdir "./vhdocl-dist";
+
+exec "./install-vhdocl.pl", @ARGV;
+
